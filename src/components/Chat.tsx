d7b313e184a3df0117f67c8678aa231c84cf196a
Koffi-exe <adithyaasasi@gmail.com>
2025-06-10 14:45:05 +0530

    global chat: update

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState } from "react";
-import { Navigate, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { io, Socket } from "socket.io-client";
 
 // Define the message type
